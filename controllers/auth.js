d368e070bad3117ec6ae71b89647ddc8962560e5
royce-u <royceubando@gmail.com>
2020-04-22 12:36:30 -0700

    signup form implemented w/ preserved data on pwd mismatch

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,7 @@
 //node modules/variables
 router = require('express').Router()
 
-//routes
+//GET /auth/login - this is a page that renders login form
 router.get('/login', (req, res) => {
     res.render('auth/login')
 })
@@ -10,9 +10,22 @@ router.post('/login', (req, res) => {
     console.log('DATA', req.body)
     res.send('Hello from the post route')
 })
-
+//GET /auth/signup - this is a page that renders the signup form
 router.get('/signup', (req, res) => {
-    res.render('auth/signup')
+    res.render('auth/signup', { data: {} })
+})
+
+//POST /auth./signup
+router.post('/signup', (req, res) => {
+    if (req.body.password !== req.body.password_verify) {
+        //send error msg about passwords not matching
+        //put user back onto signup form to resubmit
+        console.log('passwords do not match')
+        res.render('auth/signup', { data: req.body })
+    }
+    else {
+        res.send('successsss')
+    }
 })
 
 //export - allows to include in another page
